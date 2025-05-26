9a40ff5bae50c20f15741d66d2ad3620bf9fc634
Fabricio Verhagen <fabricioverhagen@gmail.com>
2025-05-25 21:02:28 -0300

    calculo imc completada

--- calculadora_indice.py
+++
@@ -1,5 +0,0 @@
-def calcular_imc ():
-    peso = float(input("Coloque su peso en kg:"))
-    altura = float(input("Colocar su altura en metros:"))
-    imc_retorno = peso /altura**2
-    return imc_retorno
